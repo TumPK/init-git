b353af06e575b414bd67791d59c540fbe3fc80e4
tumscorpion <pk.prateep@gmail.com>
2017-09-11 12:54:18 +0700

    print number 1-9

--- src/Hello.java
+++ src/Hello.java
@@ -4,9 +4,10 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("test");
+		
+		for(int i=0; i < 10; i++) {
+			System.out.println("NO."+i);
+		}
 	}
 
 }
